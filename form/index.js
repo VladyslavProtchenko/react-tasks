370be00c85b239ad7ec956e7bb5be453384f47f5
vladyslav <vladyslav25cm@gmail.com>
2022-11-24 00:00:20 +0200

    final

--- form/index.js
+++ form/index.js
@@ -1,5 +1,7 @@
 let sign = document.querySelector('#sign_in');
 let signUp = document.querySelector('#sign_up');
+let user = document.querySelector('#username').value;
+let pass = document.querySelector('#password').value;
 
     sign.onclick = function() {
         if(!sign.classList.contains('active')) {
@@ -10,6 +12,10 @@ let signUp = document.querySelector('#sign_up');
             sign.classList.remove('active');
             signUp.classList.add('active');
         }
+        document.querySelector('#label_pass').classList.remove('hide');
+        document.querySelector('#password').classList.remove('hide');
+        document.querySelector('#save').classList.remove('hide');
+        document.querySelector('#fp').classList.remove('hide');
     }
     signUp.onclick = function() {
         if(!signUp.classList.contains('active')) {
@@ -20,8 +26,13 @@ let signUp = document.querySelector('#sign_up');
             sign.classList.add('active');
             signUp.classList.remove('active');
         }
-    }
 
+        document.querySelector('#label_pass').classList.add('hide');
+        document.querySelector('#password').classList.add('hide');
+        document.querySelector('#save').classList.add('hide');
+        document.querySelector('#fp').classList.add('hide');
+        document.querySelector('#btn').innerHTML ="Sing up";
+    }
     
 function block(){
     let btn = document.querySelector('#btn');
@@ -45,9 +56,7 @@ let check = document.querySelector('#check__label');
 check.addEventListener('mouseover', (event) => {
     checkBtn();
 })
-btn.addEventListener('mouseover', (event) => {
-    checkBtn();
-})
+
     
 btn.onclick = function(){
     let userData = document.querySelector('#username').value;
@@ -57,10 +66,27 @@ btn.onclick = function(){
         "password" : passData
     }
     console.log(data);
+    document.querySelector('#username').value = '';
+    document.querySelector('#password').value = '';
 }
 document.querySelector('#fp').onclick = ()=>{
     alert('I\'m so sorry, stupid Bitch, gerrara here!!!');
 }
+
+document.querySelector('#username').onclick = function(){
+    document.querySelector('#label_name').classList.add('active');
+    document.querySelector('#username').classList.add('input_active')
+}
+document.querySelector('#password').onclick = function(){
+    document.querySelector('#password').classList.add('input_active');
+    document.querySelector('#label_pass').classList.add('active');
+
+}
+
+
+
+
+
 
 
 
